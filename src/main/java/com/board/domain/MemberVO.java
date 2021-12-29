cd162f3b1007ce61f1a6535c4ae89e537a5bb077
yeongchan <skadudcks321@naver.com>
2021-12-29 19:41:18 +0900

    start

---
+++ src/main/java/com/board/domain/MemberVO.java
@@ -0,0 +1,34 @@
+package com.board.domain;
+
+public class MemberVO {
+	private int uno;
+	private String userID;
+	private String userPW;
+	private String userEmail;
+	public int getUno() {
+		return uno;
+	}
+	public void setUno(int uno) {
+		this.uno = uno;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPW() {
+		return userPW;
+	}
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+
+}
